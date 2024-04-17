0e062a220b7997d538343a35e9e2bbe7662747ee
andresMurillo23 <andres.murillo21@ucr.ac.cr>
2024-04-17 14:28:58 -0600

    Correcciones casi finales de TP1

--- Tarea programada #1 - Entrega #1/main.cpp
+++ Tarea programada #1 - Entrega #1/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <chrono> // Para medir el tiempo
-#include <random> // Para generar números aleatorios
+#include <random> // Generar números aleatorios
 #include "Ordenador.h"
 
 int main() {
     Ordenador ordenador;
     const int num_arreglos = 1;
-    const int tam_arreglo = 25000; // Tamaño del arreglo
+    const int tam_arreglo = 25000;
 
     // Generar números aleatorios
     std::random_device rd;
@@ -15,23 +15,17 @@ int main() {
 
     for (int i = 0; i < num_arreglos; ++i) {
         int* arreglo = new int[tam_arreglo];
-        // Llenar el arreglo con números aleatorios
         for (int j = 0; j < tam_arreglo; ++j) {
             arreglo[j] = dis(gen);
-            
         }
 
         // Medir el tiempo de ejecución del algoritmo de ordenamiento
-        auto start = std::chrono::high_resolution_clock::now(); // Iniciar el temporizador
-        ordenador.seleccion(arreglo, tam_arreglo); // Llamar al algoritmo de ordenamiento
-        auto end = std::chrono::high_resolution_clock::now(); // Detener el temporizador
-
-        // Calcular la duración en milisegundos
+        auto start = std::chrono::high_resolution_clock::now();
+        ordenador.seleccion(arreglo, tam_arreglo);
+        auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> duration = end - start;
-        // Imprimir la duración de la ejecución
-        std::cout << "Duracion de ejecucion " << i + 1 << ": " << duration.count() << " ms" << std::endl;
 
-        // Limpiar el arreglo dinámico
+        std::cout << "Duracion de ejecucion " << i + 1 << ": " << duration.count() << " ms" << std::endl;
         delete[] arreglo;
     }
 
