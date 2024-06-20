d680012b86ae6d1b230bfe8ee5a43a785074b66c
andresMurillo23 <andres.murillo21@ucr.ac.cr>
2024-06-20 00:03:07 -0600

    Correcciones en codigo y se comienzan a realizar pruebas

--- tareas_programadas/tp2/entrega2/src/chasht.h
+++ tareas_programadas/tp2/entrega2/src/chasht.h
@@ -1,41 +1,69 @@
-#ifndef chasht_h
-#define chasht_h
+//
+//  chtable.h
+//
 
+#ifndef chtable_h
+#define chtable_h
+
+#include <list>
 #include <vector>
-#include "llistd.h"
+#include <stdexcept>
+#include "llistD.h"
 
 template <typename T>
+
+// Tabla de dispersión con encadenamiento
 class chtable {
 public:
     // Constructor que especifica el tamaño de la tabla
     chtable(int sz) : size(sz), table(sz) {}
-
+        
     // Destructor (borra la tabla)
     ~chtable() {}
 
     // Inserta el elemento en la tabla
     void Insert(const T& k) {
-        int q = hashFunction(k);
-        table[q].Insert(new llnode<T>(k));
+        int i = 0;
+        int q;
+        do {
+            q = hashFunction(k, i);
+            if (table[q].Search(k) == nullptr) {
+                table[q].Insert(new llnode<T>(k));
+                return;
+            }
+            i++;
+        } while (i < size);
+        throw std::overflow_error("hash table overflow");
     }
 
     // Retorna un puntero a la llave o nullptr si no se encuentra
     T* Search(const T& k) {
-        int q = hashFunction(k);
-        llnode<T>* node = table[q].Search(k);
-        if (node != nullptr && node->getKey() == k) {
-            return &node->getKeyRef();
-        }
+        int i = 0;
+        int q;
+        do {
+            q = hashFunction(k, i);
+            llnode<T>* node = table[q].Search(k);
+            if (node != nullptr && node->getKey() == k) {
+                return &node->getKeyRef();
+            }
+            i++;
+        } while (i < size && table[q].Search(k) != nullptr);
         return nullptr;
     }
 
     // Elimina el elemento de la tabla
     void Delete(const T& k) {
-        int q = hashFunction(k);
-        llnode<T>* node = table[q].Search(k);
-        if (node != nullptr && node->getKey() == k) {
-            table[q].Delete(node);
-        }
+        int i = 0;
+        int q;
+        do {
+            q = hashFunction(k, i);
+            llnode<T>* node = table[q].Search(k);
+            if (node != nullptr && node->getKey() == k) {
+                table[q].Delete(node);
+                return;
+            }
+            i++;
+        } while (i < size && table[q].Search(k) != nullptr);
     }
 
     // Imprime los datos de la tarea
@@ -45,12 +73,12 @@ public:
 
 private:
     // Función de dispersión
-    int hashFunction(const T& k) const {
-        return k % size;
+    int hashFunction(const T& k, int i) const {
+        return (k + i) % size;
     }
 
     int size; // Número de entradas en la tabla
     std::vector<llist<T>> table; // La tabla es un vector de listas enlazadas
 };
 
-#endif /* chasht_h */
+#endif /* chtable_h */
--- tareas_programadas/tp2/entrega2/src/main.cpp
+++ tareas_programadas/tp2/entrega2/src/main.cpp
@@ -90,145 +90,48 @@
 // }
  ///////////////////////////////////////////////////////////////
 
-// #include <iostream>
-// #include <chrono>
-// #include <random>
-// #include "chasht.h"
-
-// const int N = 1000000;
-// const int E = 10000;
-// const int REPETITIONS = 3;
-
-// void insercionAleatoria(chtable<int>& tabla) {
-//     std::random_device rd;
-//     std::mt19937 gen(rd());
-//     std::uniform_int_distribution<> dis(0, 2 * N);
-
-//     for (int i = 0; i < N; ++i) {
-//         int key = dis(gen);
-//         tabla.Insert(key);
-//     }
-// }
-
-// void insercionOrdenada(chtable<int>& tabla) {
-//     for (int i = 0; i < N; ++i) {
-//         tabla.Insert(i);
-//     }
-// }
-
-// void buscarElementos(chtable<int>& tabla) {
-//     std::random_device rd;
-//     std::mt19937 gen(rd());
-//     std::uniform_int_distribution<> dis(0, 2 * N);
-
-//     for (int i = 0; i < E; ++i) {
-//         int key = dis(gen);
-//         tabla.Search(key);
-//     }
-// }
-
-// void medirTiempoInsercionAleatoria() {
-//     for (int i = 0; i < REPETITIONS; ++i) {
-//         chtable<int> tabla(N);
-//         auto start = std::chrono::high_resolution_clock::now();
-//         insercionAleatoria(tabla);
-//         auto end = std::chrono::high_resolution_clock::now();
-//         std::chrono::duration<double> duration = end - start;
-//         std::cout << "Tiempo de insercion aleatoria (repeticion " << i + 1 << "): " << duration.count() << " segundos" << std::endl;
-//     }
-// }
-
-// void medirTiempoInsercionOrdenada() {
-//     for (int i = 0; i < REPETITIONS; ++i) {
-//         chtable<int> tabla(N);
-//         auto start = std::chrono::high_resolution_clock::now();
-//         insercionOrdenada(tabla);
-//         auto end = std::chrono::high_resolution_clock::now();
-//         std::chrono::duration<double> duration = end - start;
-//         std::cout << "Tiempo de insercion ordenada (repeticion " << i + 1 << "): " << duration.count() << " segundos" << std::endl;
-//     }
-// }
-
-// void medirTiempoBusqueda(chtable<int>& tabla) {
-//     auto start = std::chrono::high_resolution_clock::now();
-//     buscarElementos(tabla);
-//     auto end = std::chrono::high_resolution_clock::now();
-//     std::chrono::duration<double> duration = end - start;
-//     std::cout << "Tiempo de busqueda: " << duration.count() << " segundos" << std::endl;
-// }
-
-// int main() {
-//     std::cout << "Pruebas de inserción aleatoria:" << std::endl;
-//     medirTiempoInsercionAleatoria();
-
-//     std::cout << "\nPruebas de insercion ordenada:" << std::endl;
-//     medirTiempoInsercionOrdenada();
-
-//     std::cout << "\nPruebas de búsqueda tras insercion aleatoria:" << std::endl;
-//     for (int i = 0; i < REPETITIONS; ++i) {
-//         chtable<int> tabla(N);
-//         insercionAleatoria(tabla);
-//         medirTiempoBusqueda(tabla);
-//     }
-
-//     std::cout << "\nPruebas de búsqueda tras insercion ordenada:" << std::endl;
-//     for (int i = 0; i < REPETITIONS; ++i) {
-//         chtable<int> tabla(N);
-//         insercionOrdenada(tabla);
-//         medirTiempoBusqueda(tabla);
-//     }
-
-//     return 0;
-// }
-  
-
-
-  ////////////////////////////////////////////////////////////////////////////////
-
 #include <iostream>
 #include <chrono>
 #include <random>
-#include "rbtree.h"
+#include "chasht.h"
 
 const int N = 1000000;
 const int E = 10000;
 const int REPETITIONS = 3;
 
-void insercionAleatoria(rbtree<int>& arbol) {
+void insercionAleatoria(chtable<int>& tabla) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, 2 * N);
 
     for (int i = 0; i < N; ++i) {
         int key = dis(gen);
-        rbtnode<int> *nuevoNodo = new rbtnode<int>(key, nullptr, arbol.nil, arbol.nil);
-        arbol.Insert(nuevoNodo);
+        tabla.Insert(key);
     }
 }
 
-void insercionOrdenada(rbtree<int>& arbol) {
+void insercionOrdenada(chtable<int>& tabla) {
     for (int i = 0; i < N; ++i) {
-        rbtnode<int> *nuevoNodo = new rbtnode<int>(i, nullptr, arbol.nil, arbol.nil);
-        arbol.Insert(nuevoNodo);
+        tabla.Insert(i);
     }
 }
 
-void buscarElementos(rbtree<int>& arbol) {
+void buscarElementos(chtable<int>& tabla) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, 2 * N);
 
     for (int i = 0; i < E; ++i) {
         int key = dis(gen);
-        arbol.Search(arbol.root, key);
+        tabla.Search(key);
     }
 }
 
 void medirTiempoInsercionAleatoria() {
     for (int i = 0; i < REPETITIONS; ++i) {
-        rbtree<int> arbol;
+        chtable<int> tabla(N);
         auto start = std::chrono::high_resolution_clock::now();
-        insercionAleatoria(arbol);
+        insercionAleatoria(tabla);
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> duration = end - start;
         std::cout << "Tiempo de insercion aleatoria (repeticion " << i + 1 << "): " << duration.count() << " segundos" << std::endl;
@@ -237,18 +140,18 @@ void medirTiempoInsercionAleatoria() {
 
 void medirTiempoInsercionOrdenada() {
     for (int i = 0; i < REPETITIONS; ++i) {
-        rbtree<int> arbol;
+        chtable<int> tabla(N);
         auto start = std::chrono::high_resolution_clock::now();
-        insercionOrdenada(arbol);
+        insercionOrdenada(tabla);
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> duration = end - start;
         std::cout << "Tiempo de insercion ordenada (repeticion " << i + 1 << "): " << duration.count() << " segundos" << std::endl;
     }
 }
 
-void medirTiempoBusqueda(rbtree<int>& arbol) {
+void medirTiempoBusqueda(chtable<int>& tabla) {
     auto start = std::chrono::high_resolution_clock::now();
-    buscarElementos(arbol);
+    buscarElementos(tabla);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
     std::cout << "Tiempo de busqueda: " << duration.count() << " segundos" << std::endl;
@@ -263,17 +166,115 @@ int main() {
 
     std::cout << "\nPruebas de búsqueda tras insercion aleatoria:" << std::endl;
     for (int i = 0; i < REPETITIONS; ++i) {
-        rbtree<int> arbol;
-        insercionAleatoria(arbol);
-        medirTiempoBusqueda(arbol);
+        chtable<int> tabla(N);
+        insercionAleatoria(tabla);
+        medirTiempoBusqueda(tabla);
     }
 
     std::cout << "\nPruebas de búsqueda tras insercion ordenada:" << std::endl;
     for (int i = 0; i < REPETITIONS; ++i) {
-        rbtree<int> arbol;
-        insercionOrdenada(arbol);
-        medirTiempoBusqueda(arbol);
+        chtable<int> tabla(N);
+        insercionOrdenada(tabla);
+        medirTiempoBusqueda(tabla);
     }
 
     return 0;
 }
+  
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+
+// #include <iostream>
+// #include <chrono>
+// #include <random>
+// #include "rbtree.h"
+
+// const int N = 1000000;
+// const int E = 10000;
+// const int REPETITIONS = 3;
+
+// void insercionAleatoria(rbtree<int>& arbol) {
+//     std::random_device rd;
+//     std::mt19937 gen(rd());
+//     std::uniform_int_distribution<> dis(0, 2 * N);
+
+//     for (int i = 0; i < N; ++i) {
+//         int key = dis(gen);
+//         rbtnode<int> *nuevoNodo = new rbtnode<int>(key, nullptr, arbol.nil, arbol.nil);
+//         arbol.Insert(nuevoNodo);
+//     }
+// }
+
+// void insercionOrdenada(rbtree<int>& arbol) {
+//     for (int i = 0; i < N; ++i) {
+//         rbtnode<int> *nuevoNodo = new rbtnode<int>(i, nullptr, arbol.nil, arbol.nil);
+//         arbol.Insert(nuevoNodo);
+//     }
+// }
+
+// void buscarElementos(rbtree<int>& arbol) {
+//     std::random_device rd;
+//     std::mt19937 gen(rd());
+//     std::uniform_int_distribution<> dis(0, 2 * N);
+
+//     for (int i = 0; i < E; ++i) {
+//         int key = dis(gen);
+//         arbol.Search(arbol.root, key);
+//     }
+// }
+
+// void medirTiempoInsercionAleatoria() {
+//     for (int i = 0; i < REPETITIONS; ++i) {
+//         rbtree<int> arbol;
+//         auto start = std::chrono::high_resolution_clock::now();
+//         insercionAleatoria(arbol);
+//         auto end = std::chrono::high_resolution_clock::now();
+//         std::chrono::duration<double> duration = end - start;
+//         std::cout << "Tiempo de insercion aleatoria (repeticion " << i + 1 << "): " << duration.count() << " segundos" << std::endl;
+//     }
+// }
+
+// void medirTiempoInsercionOrdenada() {
+//     for (int i = 0; i < REPETITIONS; ++i) {
+//         rbtree<int> arbol;
+//         auto start = std::chrono::high_resolution_clock::now();
+//         insercionOrdenada(arbol);
+//         auto end = std::chrono::high_resolution_clock::now();
+//         std::chrono::duration<double> duration = end - start;
+//         std::cout << "Tiempo de insercion ordenada (repeticion " << i + 1 << "): " << duration.count() << " segundos" << std::endl;
+//     }
+// }
+
+// void medirTiempoBusqueda(rbtree<int>& arbol) {
+//     auto start = std::chrono::high_resolution_clock::now();
+//     buscarElementos(arbol);
+//     auto end = std::chrono::high_resolution_clock::now();
+//     std::chrono::duration<double> duration = end - start;
+//     std::cout << "Tiempo de busqueda: " << duration.count() << " segundos" << std::endl;
+// }
+
+// int main() {
+//     std::cout << "Pruebas de inserción aleatoria:" << std::endl;
+//     medirTiempoInsercionAleatoria();
+
+//     std::cout << "\nPruebas de insercion ordenada:" << std::endl;
+//     medirTiempoInsercionOrdenada();
+
+//     std::cout << "\nPruebas de búsqueda tras insercion aleatoria:" << std::endl;
+//     for (int i = 0; i < REPETITIONS; ++i) {
+//         rbtree<int> arbol;
+//         insercionAleatoria(arbol);
+//         medirTiempoBusqueda(arbol);
+//     }
+
+//     std::cout << "\nPruebas de búsqueda tras insercion ordenada:" << std::endl;
+//     for (int i = 0; i < REPETITIONS; ++i) {
+//         rbtree<int> arbol;
+//         insercionOrdenada(arbol);
+//         medirTiempoBusqueda(arbol);
+//     }
+
+//     return 0;
+// }
+
